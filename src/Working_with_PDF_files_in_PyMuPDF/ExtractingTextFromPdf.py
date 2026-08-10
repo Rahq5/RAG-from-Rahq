e2e1918700693d7aaf5059fc7eaa1803375ebd1e
RaHQ_05 <Rawihk2005@gmail.com>
2026-08-10 10:50:05 +0300

    missing with textFromPDF code

--- src/Working_with_PDF_files_in_PyMuPDF/ExtractingTextFromPdf.py
+++ src/Working_with_PDF_files_in_PyMuPDF/ExtractingTextFromPdf.py
@@ -5,7 +5,7 @@ doc = pymupdf.open("Working_with_PDF_files_in_PyMuPDF/a.pdf") # opens a document
 out = open("output.txt", "wb") # creating text output
 
 for page in doc : # for each page in the doc
-    text = page.get_text().encode("utf-8") # get plain text that is in utf-8
+    text = page.get_text("text",sort=True).encode("utf-8") # get plain text that is in utf-8
     out.write(text) # write text of page
     out.write(bytes((12,))) # write the page delimeter (some sort of "go to next page")
 out.close()
